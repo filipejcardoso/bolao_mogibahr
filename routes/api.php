a6cfb6d0be6c810b70e257df13d6cae3ca2550fb
filipejcardoso <filipejcardoso@eletrobidu.com.br>
2018-06-22 01:53:37 -0300

    fix hacking

--- routes/api.php
+++ routes/api.php
@@ -6,14 +6,14 @@ Route::group(['prefix' =>'participantes'],function()
 {
 	Route::get('', ['uses' => 'ParticipantesController@index']);
 	Route::get('{id}', ['uses' => 'ParticipantesController@show']);
-	Route::post('', ['uses' => 'ParticipantesController@store']);
-	Route::patch('{id}', ['uses' => 'ParticipantesController@update']);
-	Route::delete('{id}', ['uses' => 'ParticipantesController@destroy']);
+	// Route::post('', ['uses' => 'ParticipantesController@store']);
+	// Route::patch('{id}', ['uses' => 'ParticipantesController@update']);
+	// Route::delete('{id}', ['uses' => 'ParticipantesController@destroy']);
 	
 	Route::group(['prefix' =>'/{participantes}/apostas'],function()
 	{
 		Route::get('', ['uses' => 'ApostasController@index']);
-		Route::patch('{id}', ['uses' => 'ApostasController@update']);
+		// Route::patch('{id}', ['uses' => 'ApostasController@update']);
 	});
 });
 Route::group(['prefix' =>'/jogos'],function()
