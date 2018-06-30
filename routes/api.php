191da3a7b518ae8fe8630f6ec988759c0500a27c
filipejcardoso <filipejcardoso@eletrobidu.com.br>
2018-06-30 11:11:47 -0300

    start

--- routes/api.php
+++ routes/api.php
@@ -6,20 +6,20 @@ Route::group(['prefix' =>'participantes'],function()
 {
 	Route::get('', ['uses' => 'ParticipantesController@index']);
 	Route::get('{id}', ['uses' => 'ParticipantesController@show']);
-	Route::post('', ['uses' => 'ParticipantesController@store']);
-	Route::patch('{id}', ['uses' => 'ParticipantesController@update']);
+	// Route::post('', ['uses' => 'ParticipantesController@store']);
+	// Route::patch('{id}', ['uses' => 'ParticipantesController@update']);
 	// Route::delete('{id}', ['uses' => 'ParticipantesController@destroy']);
 	
 	Route::group(['prefix' =>'/{participantes}/apostas'],function()
 	{
 		Route::get('', ['uses' => 'ApostasController@index']);
-		Route::patch('{id}', ['uses' => 'ApostasController@update']);
+		// Route::patch('{id}', ['uses' => 'ApostasController@update']);
 	});
 });
 Route::group(['prefix' =>'/jogos'],function()
 {
 	Route::get('', ['uses' => 'JogosController@index']);
-	Route::post('', ['uses' => 'JogosController@store']);
+	// Route::post('', ['uses' => 'JogosController@store']);
 	Route::patch('{id}', ['uses' => 'JogosController@update']);
 });
 
