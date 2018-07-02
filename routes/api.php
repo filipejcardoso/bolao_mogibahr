75fc0eb75da3cfbf59ef3da5669d963d0b178694
filipejcardoso <filipejcardoso@eletrobidu.com.br>
2018-07-01 22:27:06 -0300

    api insert

--- routes/api.php
+++ routes/api.php
@@ -7,13 +7,13 @@ Route::group(['prefix' =>'participantes'],function()
 	Route::get('', ['uses' => 'ParticipantesController@index']);
 	Route::get('{id}', ['uses' => 'ParticipantesController@show']);
 	// Route::post('', ['uses' => 'ParticipantesController@store']);
-	// Route::patch('{id}', ['uses' => 'ParticipantesController@update']);
+	Route::patch('{id}', ['uses' => 'ParticipantesController@update']);
 	// Route::delete('{id}', ['uses' => 'ParticipantesController@destroy']);
 	
 	Route::group(['prefix' =>'/{participantes}/apostas'],function()
 	{
 		Route::get('', ['uses' => 'ApostasController@index']);
-		// Route::patch('{id}', ['uses' => 'ApostasController@update']);
+		Route::patch('{id}', ['uses' => 'ApostasController@update']);
 	});
 });
 Route::group(['prefix' =>'/jogos'],function()
